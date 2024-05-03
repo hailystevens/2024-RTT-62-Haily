6b95d0349dc26e8c7f0679871d57e45a90bcc967
Haily S <Hailybellestevens@gmail.com>
2024-05-03 15:11:05 -0400

    303.4.1 tweaks

--- JavaClasswork/src/main/java/org/example/Classwork/Slides_303_4/ForLoopExample.java
+++ JavaClasswork/src/main/java/org/example/Classwork/Slides_303_4/ForLoopExample.java
@@ -1,5 +1,8 @@
 package org.example.Classwork.Slides_303_4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ForLoopExample {
     public static void main(String[] args) {
         for (int count = 0; count < 10; count++) {
@@ -11,3 +14,29 @@ public class ForLoopExample {
         }
     }
 }
+
+class EnhancedLoop {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("First Element");
+        stringList.add("Two");
+        stringList.add("Three");
+        stringList.add("Four");
+        stringList.add("Five");
+
+        //we can use an old style loop to move across this list
+        for (int pos = 0; pos < stringList.size(); pos++) {
+            System.out.println(stringList.get(pos));
+        }
+        System.out.println("------------------------");
+
+        //nice for new loop
+        for (String element : stringList) {
+            if (element.equals("Three")) {
+                continue;
+            }
+            System.out.println(element);
+        }
+    }
+}
+
