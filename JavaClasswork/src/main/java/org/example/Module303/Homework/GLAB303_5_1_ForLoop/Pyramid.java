81e094fd5fe7f15ba428744b63a73bd49dea68b0
Haily S <Hailybellestevens@gmail.com>
2024-05-06 06:23:03 -0400

    completed assignments

--- JavaClasswork/src/main/java/org/example/Module303/Homework/GLAB303_5_1_ForLoop/Pyramid.java
+++ JavaClasswork/src/main/java/org/example/Module303/Homework/GLAB303_5_1_ForLoop/Pyramid.java
@@ -2,11 +2,39 @@ package org.example.Module303.Homework.GLAB303_5_1_ForLoop;
 
 public class Pyramid {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.println("* ");
-            }
-            System.out.println(); //new line
-        }
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        //haha jk
     }
 }
+//       The output for this code didnt work for me:
+//        for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.println("* ");
+//            }
+//            System.out.println(); //new line
+//        }
+        //Output:
+            //*
+
+            //*
+            //*
+
+            //*
+            //*
+            //*
+
+            //*
+            //*
+            //*
+            //*
+
+            //*
+            //*
+            //*
+            //*
+            //*
+
