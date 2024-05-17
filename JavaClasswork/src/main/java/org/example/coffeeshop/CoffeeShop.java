ce5eed018bc0b5d63036d22a57198de886512cea
Haily S <Hailybellestevens@gmail.com>
2024-05-17 01:32:36 -0400

    classwork

--- JavaClasswork/src/main/java/org/example/coffeeshop/CoffeeShop.java
+++ JavaClasswork/src/main/java/org/example/coffeeshop/CoffeeShop.java
@@ -5,36 +5,41 @@ import java.util.List;
 
 public class CoffeeShop {
 
-    private List<Products> products = new ArrayList<>();
+    private List<Product> products = new ArrayList<>();
 
     // building the menu of products for sale at the coffee shop
     public void initProducts() {
-        Products p1 = new Products();
+        ProductReader pr = new ProductReader();
+        List<Product> productsFromFile = pr.readProducts();
+        products.addAll(productsFromFile);
+
+        Product p1 = new Product();
         p1.setPrice(5.46);
         p1.setName("Small Coffee");
         p1.setBeverage(true);
         products.add(p1);
 
-        Products largeCoffee = new Products();
+        Product largeCoffee = new Product();
         largeCoffee.setPrice(9.46);
         largeCoffee.setName("Large Coffee");
         largeCoffee.setBeverage(true);
         products.add(largeCoffee);
 
-        Products cakePop = new Products();
+        Product cakePop = new Product();
         cakePop.setPrice(4.89);
         cakePop.setName("Cake Pop");
         cakePop.setBeverage(false);
         products.add(cakePop);
 
-        Products eggSandwich = new Products(7.89, "Egg Sandwich", false, 50);
+        Product eggSandwich = new Product(7.89, "Egg Sandwich", false, 50);
         products.add(eggSandwich);
+
     }
 
     // print the list of products available for sale
     public void printProducts() {
         for (int pos = 0; pos < products.size(); pos++) {
-            Products product = products.get(pos);
+            Product product = products.get(pos);
             System.out.println((pos + 1) + ") " + product.getName() + "\t" + product.getPrice());
         }
     }
